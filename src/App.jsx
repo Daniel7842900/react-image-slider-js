8d4e45765b06e7b862fde37887072250dbb01b78
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-07-16 22:14:32 -0700

    fetch image and render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { AiOutlineLeftCircle, AiOutlineRightCircle } from "react-icons/ai";
+import "./App.css";
+
+const imageEndPoint = "https://picsum.photos/350/250";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const getImages = async () => {
+    const response = await axios.get(imageEndPoint);
+    const responseURL = response.request.responseURL;
+    setData(responseURL);
+  };
+
   return (
     <>
       <div className="greetings-container">
@@ -11,7 +27,9 @@ function App() {
         <div className="previous-btn-container">
           <AiOutlineLeftCircle className="navigation-btn" />
         </div>
-        <div className="main-image-container"></div>
+        <div className="main-image-container">
+          {data && <img src={data} alt="Image" />}
+        </div>
         <div className="next-btn-container">
           <AiOutlineRightCircle className="navigation-btn" />
         </div>
